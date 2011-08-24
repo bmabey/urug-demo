a7c988c1b165b3d43312f05ad81b9a71a9669220
Ben Mabey <ben@benmabey.com>
2011-08-23 19:32:09 -0600

    adds enumerating solution

--- word_break.rb
+++ word_break.rb
@@ -1,6 +1,11 @@
 #/usr/bin/env ruby
 require 'set'
 require 'rspec'
+require 'enumerating'
+
+module Enumerable
+  alias mapping collecting # enumerating defines collecting.. but I prefer mapping...
+end
 
 # Given an input string and a dictionary of words,
 # segment the input string into a space-separated
@@ -12,7 +17,30 @@ require 'rspec'
 module WordBreak
   class << self
     def segmentize(string, dictionary)
+      segmentize = memoize( -> string {
+        return string if dictionary.include?(string)
+
+        prefixes = (0...(string.size)).mapping { |i| [string[0..i], i]}
+
+        candidates = prefixes.selecting { |(prefix,i)| dictionary.include?(prefix) }
+
+        solutions = candidates.mapping { |(prefix,i)|
+            remaining_segment = segmentize(string[(i+1)..-1], dictionary)
+            [prefix, remaining_segment].join(" ") if remaining_segment }.
+          rejecting {|r| r.nil? }
+
+        solutions.first })
+
+      segmentize.call(string)
     end
+
+    private
+
+    def memoize(fn)
+      cache = {}
+      -> *args { cache.include?(args) ? cache[args] : cache[args] = fn.call(*args) }
+    end
+
   end
 end
 
